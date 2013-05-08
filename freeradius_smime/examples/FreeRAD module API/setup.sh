a84526ddbbe1a420d0aeb061bf9b415215869506
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-08 12:05:49 +0200

    Bash scripts are ugly

--- freeradius_smime/examples/FreeRAD module API/setup.sh
+++ freeradius_smime/examples/FreeRAD module API/setup.sh
@@ -12,6 +12,7 @@ cd /usr/src
 wget ftp://ftp.freeradius.org/pub/freeradius/freeradius-server-2.1.12.tar.gz
 tar -xzf freeradius-server-2.1.12.tar.gz
 rm -f freeradius-server-2.1.12.tar.gz
+sleep 0.5
 cd freeradius-server-2.1.12
 
 ./configure
@@ -27,14 +28,19 @@ cd ./rlm_testing
 mv rlm_example.c rlm_testing.c
 
 cat configure.in | awk '{gsub("example", "testing"); print}' > configure.in.tmp
+sleep 0.5
 cat Makefile.in | awk '{gsub("example", "testing"); print}' > Makefile.in.tmp
+sleep 0.5
 cat Makefile.clean | awk '{gsub("example", "testing"); print}' > Makefile.clean.tmp
+sleep 0.5
 cat rlm_testing.c | awk '{gsub("example", "testing"); print}' > rlm_testing.c.tmp
+sleep 0.5
 
 mv -f configure.in.tmp configure.in
 mv -f Makefile.in.tmp Makefile.in
 mv -f Makefile.clean.tmp Makefile.clean
 mv -f rlm_testing.c.tmp rlm_testing.c
+sleep 1
 
 autoconf
 ./configure
