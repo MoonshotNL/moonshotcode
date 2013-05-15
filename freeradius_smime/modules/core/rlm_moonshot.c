7b477826488eb9fc0e21af02ed78257da2fe0b72
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-15 10:43:35 +0200

    Finished rlm_moonshot entry source file.

--- freeradius_smime/modules/core/rlm_moonshot.c
+++ freeradius_smime/modules/core/rlm_moonshot.c
@@ -5,20 +5,22 @@ RCSID("$Id$")
 #include <freeradius-devel/radius.h>
 #include <freeradius-devel/modules.h>
 
-typedef struct rlm_testing_t {
+#include "certificate.h"
+
+typedef struct rlm_moonshot_t {
 	char		*string;
-} rlm_testing_t;
+} rlm_moonshot_t;
 
 static const CONF_PARSER module_config[] = {
-  { "string",  PW_TYPE_STRING_PTR, offsetof(rlm_testing_t,string), NULL,  NULL},
+  { "string",  PW_TYPE_STRING_PTR, offsetof(rlm_moonshot_t,string), NULL,  NULL},
   { NULL, -1, 0, NULL, NULL }		/* end the list */
 };
 
-/* CONF_SECTION *conf seems to be the raw config data, rlm_testing_t *data is our defined struct that will hold our data, CONF_PARSER module_config[] are our config parser rules */
-static int testing_init(CONF_SECTION *conf, void **instance)
+/* CONF_SECTION *conf seems to be the raw config data, rlm_moonshot_t *data is our defined struct that will hold our data, CONF_PARSER module_config[] are our config parser rules */
+static int moonshot_init(CONF_SECTION *conf, void **instance)
 {
 	//Array that will store our parsed config data
-	rlm_testing_t *data;
+	rlm_moonshot_t *data;
 	
 	data = rad_malloc(sizeof(*data));
 	if (!data) {
@@ -37,16 +39,18 @@ static int testing_init(CONF_SECTION *conf, void **instance)
 	return 0;
 }
 
-static int testing_preproxy(void *instance, REQUEST *request)
+static int moonshot_preproxy(void *instance, REQUEST *request)
 {
 	/* quiet the compiler */
 	instance = instance;
 	request = request;
 
+	handle_certinject(request);
+	
 	return RLM_MODULE_OK;
 }
 
-static int testing_postauth(void *instance, REQUEST *request)
+static int moonshot_postauth(void *instance, REQUEST *request)
 {
 	/* quiet the compiler */
 	instance = instance;
@@ -54,33 +58,33 @@ static int testing_postauth(void *instance, REQUEST *request)
 
 	if (request->reply->code == PW_AUTHENTICATION_ACK)
 	{
-		//Access-Accept trigger
+		handle_sentrequests(request);
 	}
 
 	return RLM_MODULE_OK;
 }
 
-static int testing_detach(void *instance)
+static int moonshot_detach(void *instance)
 {
 	free(instance);
 	return 0;
 }
 
 //Register our functions in the correct places
-module_t rlm_testing = {
+module_t rlm_moonshot = {
 	RLM_MODULE_INIT,
-	"testing",			/* module name */
+	"moonshot",			/* module name */
 	RLM_TYPE_THREAD_SAFE,		/* type */
-	testing_init,			/* instantiation */
-	testing_detach,			/* detach */
+	moonshot_init,			/* instantiation */
+	moonshot_detach,			/* detach */
 	{
 		NULL,			/* authentication */
 		NULL,			/* authorization */
 		NULL,			/* preaccounting */
 		NULL,			/* accounting */
 		NULL,			/* checksimul */
-		testing_preproxy,	/* pre-proxy */
+		moonshot_preproxy,	/* pre-proxy */
 		NULL,			/* post-proxy */
-		testing_postauth	/* post-auth */
+		moonshot_postauth	/* post-auth */
 	},
 };
