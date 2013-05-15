c55eeb30df8a02e970ce60e1b85f7b7c52c03bf2
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-15 12:58:31 +0200

    Converted EOL style of sources to UNIX format

--- freeradius_smime/modules/idpmodule.c
+++ freeradius_smime/modules/idpmodule.c
@@ -1,1 +1,8 @@
-//placeholder
+#include <freeradius-devel/radiusd.h>
+#include <freeradius-devel/radius.h>
+#include <freeradius-devel/modules.h>
+
+void handle_requests(REQUEST *request)
+{
+	request->dostuff;
+}
