685c8246f81dfff158e01e0532a658617f6647d4
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-15 14:02:58 +0200

    Begin of idpmodule.c and some useful notes

--- freeradius_smime/modules/idpmodule.c
+++ freeradius_smime/modules/idpmodule.c
@@ -2,7 +2,22 @@
 #include <freeradius-devel/radius.h>
 #include <freeradius-devel/modules.h>
 
+#include "common.h"
+#include "mod_mime.h"
+
 void handle_requests(REQUEST *request)
 {
-	request->dostuff;
+	VALUE_PAIR *vp = request->packet->vps;
+	do
+	{
+		if (vp->attribute == ATTR_SMIME_REQUEST)
+		{
+			handle_request(request, vp);
+		}
+	} while ((vp = vp->next) != 0)
+}
+
+void handle_request(REQUEST *request, VALUE_PAIR *vp)
+{
+	char *data = mime_unpack_attrrequest(vp->
 }
---
+++ freeradius_smime/modules/include/common.h
@@ -0,0 +1,6 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define ATTR_SMIME_REQUEST 128
+
+#endif
