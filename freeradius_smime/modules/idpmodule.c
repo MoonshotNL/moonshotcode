95b559a3a76869f6f2c3034b62cb7f3b5ad2d12f
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-15 16:10:07 +0200

    Started on idpmodule.c

--- freeradius_smime/modules/idpmodule.c
+++ freeradius_smime/modules/idpmodule.c
@@ -1,9 +1,22 @@
 #include <freeradius-devel/radiusd.h>
 #include <freeradius-devel/radius.h>
 #include <freeradius-devel/modules.h>
+#include <freeradius-devel/libradius.h>
 
 #include "common.h"
 #include "mod_mime.h"
+
+typedef struct attr_req
+{
+	int dn_len;
+	char dn[DN_MAX_LEN];
+	
+} ATTR_REQ;
+
+void attrreq_parser(char *input, int len, ATTR_REQ *output)
+{
+	
+}
 
 void handle_requests(REQUEST *request)
 {
@@ -19,5 +32,9 @@ void handle_requests(REQUEST *request)
 
 void handle_request(REQUEST *request, VALUE_PAIR *vp)
 {
-	char *data = mime_unpack_attrrequest(vp->
+	char *data = mime_unpack_attrrequest(vp->data.octets, vp->length);
+	ATTR_REQ *attr_request = rad_malloc(sizeof(ATTR_REQ));
+	attrreq_parser(data, strnlen(data, MAX_STRING_LEN), attr_request);
+	
+	attr_request.
 }
