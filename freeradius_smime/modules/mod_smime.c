d0ae099027506302439e4b23ae4b9e0966802d24
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-23 10:14:55 +0200

    Changed pointer error

--- freeradius_smime/modules/mod_smime.c
+++ freeradius_smime/modules/mod_smime.c
@@ -7,7 +7,7 @@ int pack_mime_text(char *input, int len, char **output)
 	int base64_len;
 	char *mime_headers = "Mime-version: 1.0\nContent-Type: text/plain\nContent-Transfer-Encoding: base64\n\n";
 	int mime_headers_len = strlen(mime_headers);
-	base64_len = base64_encode(input, len, base64_input);
+	base64_len = base64_encode(input, len, &base64_input);
 
 	out_len = base64_len + mime_headers_len - 1;
 
