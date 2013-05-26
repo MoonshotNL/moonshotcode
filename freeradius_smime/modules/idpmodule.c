e2d2bafc1457a58197159ffa2dafebf979b0cc5a
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-05-26 13:45:52 +0200

    Made some fixes to the parser and started on the sending functions.

--- freeradius_smime/modules/idpmodule.c
+++ freeradius_smime/modules/idpmodule.c
@@ -7,21 +7,14 @@
 #include "mod_mime.h"
 
 #define STR_MAXLEN					1024
-#define ATTR_MAXLEN					16
 
 #define STATE_TIMESTAMP				0
-#define STATE_DN					1
-#define STATE_REQUIRED_ATTR_LEN		2
-#define STATE_REQUIRED_ATTR			3
-#define STATE_REQUESTED_ATTR_LEN	4
-#define STATE_REQUESTED_ATTR		5
-
-#ifndef DUMMY_SHOULD_BE_PROVIDED_BY_CERT_MODULE
-typedef struct pkcs
-{
-	int pubkey;
-} PKCSCERT;
-#endif
+#define STATE_PROXYDN				1
+#define STATE_SERVICEDN				2
+#define STATE_REQUIRED_ATTR_LEN		3
+#define STATE_REQUIRED_ATTR			4
+#define STATE_REQUESTED_ATTR_LEN	5
+#define STATE_REQUESTED_ATTR		6
 
 typedef struct avp_struct
 {
@@ -34,8 +27,8 @@ typedef struct avp_struct
 typedef struct attr_req_in
 {
 	unsigned long timestamp;
-	char *dn;
-	char *service;
+	char *proxydn;
+	char *servicedn;
 	int required_attr_len;
 	char **required_attr;
 	int requested_attr_len;
@@ -45,8 +38,7 @@ typedef struct attr_req_in
 typedef struct attr_req_out
 {
 	unsigned long timestamp;
-	char *dn;
-	char *service;
+	char *servicedn;
 	int provided_attr_len;
 	AVP *provided_attr;
 	int requested_attr_len;
@@ -60,11 +52,13 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 	
 	char item_tmp[STR_MAXLEN];
 	int item_len = 0;
+	int item_cur = 0;
+	
 	int attr_p = 0;
 	
 	int state = STATE_TIMESTAMP;
 	
-	while(input_cur < len)
+	while(input_cur <= len)
 	{
 		switch (state)
 		{
@@ -76,23 +70,39 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 					state++;
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
 				item_cur++;
 				input_cur++;
 				break;
-			case STATE_DN:
-				if (input[input_cur] == ":")
+			case STATE_PROXYDN:
+				if (input[input_cur] == ':')
 				{
 					item_tmp[item_cur] = '\0';
-					tmp_attr_req->dn = rad_malloc(sizeof(char) * (item_cur + 1));
-					memcpy(tmp_attr_req->dn, item_tmp, sizeof(char) * (item_cur + 1));
+					tmp_attr_req->proxydn = rad_malloc(sizeof(char) * (item_cur + 1));
+					memcpy(tmp_attr_req->proxydn, item_tmp, sizeof(char) * (item_cur + 1));
 					state++;
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
+					break;
+				}
+				item_tmp[item_cur] = input[input_cur];
+				item_cur++;
+				input_cur++;
+				break;
+			case STATE_SERVICEDN:
+				if (input[input_cur] == ':')
+				{
+					item_tmp[item_cur] = '\0';
+					tmp_attr_req->servicedn = rad_malloc(sizeof(char) * (item_cur + 1));
+					memcpy(tmp_attr_req->servicedn, item_tmp, sizeof(char) * (item_cur + 1));
+					state++;
+					input_cur++;
+					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
@@ -100,14 +110,14 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 				input_cur++;
 				break;
 			case STATE_REQUIRED_ATTR_LEN:
-				if (input[input_cur] == ":")
+				if (input[input_cur] == ':')
 				{
 					item_tmp[item_cur] = '\0';
 					tmp_attr_req->required_attr_len = (int) strtol(item_tmp, NULL, 10);
 					state++;
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
@@ -115,22 +125,22 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 				input_cur++;
 				break;
 			case STATE_REQUIRED_ATTR:
-				if (input[input_cur] == ":")
+				if (input[input_cur] == ':')
 				{
 					item_tmp[item_cur] = '\0';
 					
 					if (attr_p == 0)
 					{
 						tmp_attr_req->required_attr = rad_malloc(sizeof(char *));
-						tmp_attr_req.required_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
+						tmp_attr_req->required_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
 					}
 					else
 					{
 						tmp_attr_req->required_attr = realloc(tmp_attr_req->required_attr, sizeof(char *) * (attr_p + 1));
-						tmp_attr_req.required_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
+						tmp_attr_req->required_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
 					}
 
-					memcpy(tmp_attr_req.required_attr[attr_p], item_tmp, sizeof(char) * (item_cur + 1));
+					memcpy(tmp_attr_req->required_attr[attr_p], item_tmp, sizeof(char) * (item_cur + 1));
 					attr_p++;
 					
 					if (attr_p >= tmp_attr_req->required_attr_len)
@@ -140,7 +150,7 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 					}
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
@@ -148,14 +158,14 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 				input_cur++;
 				break;
 			case STATE_REQUESTED_ATTR_LEN:
-				if (input[input_cur] == ":")
+				if (input[input_cur] == ':')
 				{
 					item_tmp[item_cur] = '\0';
 					tmp_attr_req->requested_attr_len = (int) strtol(item_tmp, NULL, 10);
 					state++;
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
@@ -163,23 +173,23 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 				input_cur++;
 				break;
 			case STATE_REQUESTED_ATTR:
-				if (input[input_cur] == ":")
+				if (input_cur == len)
 				{
 					item_tmp[item_cur] = '\0';
 					
 					if (attr_p == 0)
 					{
 						tmp_attr_req->requested_attr = rad_malloc(sizeof(char *));
-						tmp_attr_req.requested_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
+						tmp_attr_req->requested_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
 					}
 					else
 					{
 						tmp_attr_req->requested_attr = realloc(tmp_attr_req->required_attr, sizeof(char *) * (attr_p + 1));
-						tmp_attr_req.requested_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
+						tmp_attr_req->requested_attr[attr_p] = rad_malloc(sizeof(char) * (item_cur + 1));
 					}
 
 					
-					memcpy(tmp_attr_req.requested_attr[attr_p], item_tmp, sizeof(char) * (item_cur + 1));
+					memcpy(tmp_attr_req->requested_attr[attr_p], item_tmp, sizeof(char) * (item_cur + 1));
 					attr_p++;
 					
 					if (attr_p >= tmp_attr_req->requested_attr_len)
@@ -189,7 +199,7 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 					}
 					input_cur++;
 					bzero(item_tmp, sizeof(char) * STR_MAXLEN);
-					tmp_cur = 0;
+					item_cur = 0;
 					break;
 				}
 				item_tmp[item_cur] = input[input_cur];
@@ -202,14 +212,89 @@ ATTR_REQ_IN *parse_attr_req(char *input, int len)
 	return tmp_attr_req;
 }
 
-int create_output_request(ATTR_REQ_IN *input, char *output)
+ATTR_REQ_OUT *get_attr_req_out(ATTR_REQ_IN *input)
 {
-	ATTR_REQ_OUT outstruct;
-	outstruct.timestamp = (long) time(0);
-	outstruct.dn = 
-	output = malloc(5);
-	strcpy(output, "abcd");
-	return 0;
+	ATTR_REQ_OUT *outstruct;
+	outstruct = rad_malloc(sizeof(ATTR_REQ_OUT));
+	memset(outstruct, 0, sizeof(ATTR_REQ_OUT));
+	
+	outstruct->servicedn = input->servicedn;
+	outstruct->provided_attr_len = input->required_attr_len;
+	outstruct->provided_attr = get_avps_by_attributes(input->required_attr, input->required_attr_len);
+	outstruct->requested_attr_len = input->requested_attr_len;
+	outstruct->requested_attr = input->requested_attr;
+	outstruct->timestamp = (long) time(0);
+
+	return outstruct;
+}
+
+int attr_req_out_to_string(ATTR_REQ_OUT *input, char **output)
+{
+	int i;
+	char *tpm_string; //= rad_malloc(sizeof(char) * STR_MAXLEN);
+	int total_length = 0;
+	int timestamp_strlen = 0;
+	int servicedn_strlen = 0;
+	int provided_attr_len_strlen = 0;
+	int provided_attr_strlen[input->provided_attr_len];
+	int requested_attr_len_strlen = 0;
+	int requested_attr_strlen[input->requested_attr_len];
+	
+	//Calculate the total length of the resulting string
+	
+	//input->timestamp
+	digittest = input->timestamp;
+	while (digittest != 0) { digittest /= 10; timestamp_strlen++; }
+	length += timestamp_strlen;
+	length++; //The ':' delimiter
+	
+	//input->servicedn
+	servicedn_strlen += strlen(input->servicedn);
+	length += servicedn_strlen;
+	length++;
+	
+	//input->provided_attr_len
+	digittest = (long) input->provided_attr_len;
+	while (digittest != 0) { digittest /= 10; provided_attr_len_strlen++; }
+	length += provided_attr_len_strlen;
+	length++;
+	
+	//input->provided_attr
+	for (i = 0; i < input->provided_attr_len; i++)
+	{
+		provided_attr_strlen[i] = 0;
+		provided_attr_strlen[i] = strlen(input->provided_attr[i].attribute);
+		length += provided_attr_strlen[i];
+		length++; //'='
+		length += strlen(input->provided_attr[i].value);
+		length++;
+	}
+	
+	//input->requested_attr_len
+	digittest = (long) input->requested_attr_len;
+	while (digittest != 0) { digittest /= 10; length++; }
+	length++:
+	
+	//input->requested_attr
+	for (i = 0; i < input->requested_attr_len; i++)
+	{
+		length += strlen(input->requested_attr[i]);
+		length++;
+	}
+	length++ //'\0'
+
+	sprintf(tmp_string, "%ld:", input->timestamp);
+	sprintf(tmp_string, "%s:", input->servicedn);
+	sprintf(tmp_string, "%i", input->provided_attr_len);
+	for (i = 0; i < input->provided_attr_len)
+	{
+		sprintf(tmp_string, "%s=%s:", input->provided_attr[i].attribute, input->provided_attr[i].value);
+	}
+	sprintf(tmp_string, "i", input->requested_attr_len);
+	for (i = 0; i < input->requested_attr_len; i++)
+	{
+		sprintf(
+	}
 }
 
 void idp_handle_requests(REQUEST *request)
@@ -230,9 +315,10 @@ void handle_request(REQUEST *request, VALUE_PAIR *vp)
 	int input_len;
 	char *output_data;
 	int output_len;
+	ATTR_REQ_OUT *outstruct;
 	
 	input_len = mime_unpack_attrrequest(vp->data.octets, vp->length, &input_data);
-	ATTR_REQ *attr_request = parse_attr_req(data, len);
+	ATTR_REQ *attr_request = parse_attr_req(input_data, input_len);
 	if (!attr_request)
 	{
 		return;
@@ -244,5 +330,6 @@ void handle_request(REQUEST *request, VALUE_PAIR *vp)
 		return;
 	}
 	
-	output_len = create_output_request(attr_request, &output_data);
+	outstruct = get_attr_req_out(attr_request);
+	output_len = attr_req_out_to_string(outstruct, &output_data);
 }
