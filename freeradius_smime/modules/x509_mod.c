02a742d0c3caa3c6416d3b08ea24482d3c1c0f8f
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-06-03 12:41:04 +0200

    Small changes to x509_mod.c

--- freeradius_smime/modules/x509_mod.c
+++ freeradius_smime/modules/x509_mod.c
@@ -9,19 +9,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/bio.h>
+#include <openssl/x509.h>
 
-x509 &read__public_certificate(void *instance)
+X509 *read__public_certificate(void *instance)
 {
     BIO *tbio = NULL;
-    rlm_testing_T *data = instance;
+    rlm_testing_t *data = (rlm_testing_t *)instance;
     char *cert = data->pub_key;
     tbio = BIO_new_file(cert, "r");
     
-    BIO *bio = NULL;
-    bio = BIO_new_mem_buf(tbio);
-    
     X509 *certificate;
-    certificate = PEM_read_bio_X509(bio, tbio, 0, NULL);
-    
+    certificate = PEM_read_bio_X509(tbio, NULL, 0, NULL);
+	
     return certificate;
 }
