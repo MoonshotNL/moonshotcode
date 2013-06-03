75acf9eb73f352b35486cbd57cd65a79bd1c666e
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-06-03 15:13:11 +0200

    Added comments.

--- freeradius_smime/modules/x509_mod.c
+++ freeradius_smime/modules/x509_mod.c
@@ -11,6 +11,16 @@
 #include <string.h>
 #include <openssl/pem.h>
 
+/*
+ read_public_certificate
+ 
+ Use this function to get the public certificate in X509-format.
+ This function uses the location, that is defined in the freeradius.conf or radiusd.conf, of the certificate to load in BIO.
+ In the next step it will convert it in a X509-format and returns the memory location of the X509-certificate.
+ 
+ 
+ */
+
 X509 *read_public_certificate(void *instance)
 {
     BIO *tbio = NULL;
@@ -19,7 +29,7 @@ X509 *read_public_certificate(void *instance)
     rlm_testing_t *data;
     
     data = (rlm_testing_t *)instance;
-    cert = data->pub_key;
+    cert = data->pub_key;                   //get the location of the public certificate that is defined in the configuration files
     tbio = BIO_new_file(cert, "r");
     
     certificate = PEM_read_bio_X509(tbio, NULL, 0, NULL);
@@ -32,6 +42,18 @@ X509 *read_public_certificate(void *instance)
     return certificate;
 }
 
+/*
+ read_private_certificate
+ 
+ Use this function to get the private certificate in X509-format.
+ This function uses the location, that is defined in the freeradius.conf or radiusd.conf, of the certificate to load in BIO.
+ In the next step it will convert it in a X509-format and returns the memory location of the X509-certificate.
+ Some certificates are secured by a password, therefore it reads the password from the freeradius.conf or radiusd.cnf file.
+ The password has to be defined in this configuration files for correctly reading and returning the certificate in X509-format.
+ 
+ 
+ */
+
 X509 *read_private_certificate(void *instance)
 {
     BIO *tbio = NULL;
@@ -43,7 +65,7 @@ X509 *read_private_certificate(void *instance)
     
     data = (rlm_testing_t *)instance;
     cert = data->priv_key;
-    password = data->priv_key_password;
+    password = data->priv_key_password;         //get the password of the private key that is defined in the configuration files
     
     tbio = BIO_new_file(cert, "r");
     
