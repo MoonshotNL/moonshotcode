a98a1f94062c8b9807d6fae9de2daf50b7b134b2
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-06-05 18:54:29 +0200

    Template

---
+++ template.c
@@ -0,0 +1,1 @@
+DEBUG("\tVALID: %i\n\tREFERENCES: %i\n\tNAME:%s\n\tEX_PATHLEN: %ld\n\t", private_certificate->valid, private_certificate->references, private_certificate->name, private_certificate->ex_pathlen);
