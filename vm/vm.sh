c1045684435d223adfc58e96df8d0c4745a5aa1c
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-06-07 15:48:20 +0200

    Added code for the RADIUS home server.

--- vm/vm.sh
+++ vm/vm.sh
@@ -7,9 +7,10 @@ while true
 do
 echo "Please read the README before using this script."
 echo "Is this the server one of the following choice"
-echo "Root RADIUS (root)"
-echo "LDAP server (ldap)"
-echo "Home insitution RADIUS (home)"
+echo "Root RADIUS (root)" #IP address:192..168.56.11
+echo "LDAP server (ldap)" #IP address:192..168.56.13
+echo "Home insitution RADIUS (home)" #IP address:192..168.56.12
+#Janet IP address:192..168.56.14
 echo "Please select your choice (root/ldap/home/exit)"
 read a
 
@@ -20,12 +21,12 @@ case "$a" in
 			yum -y install make autoconf gcc wget openssl-devel
 			
 			cd /etc/sysconfig/network-scripts
+			cat ifcfg-eth1 > ifcfg-eth1_old
 			sed "s/^ONBOOT=.*/ONBOOT=yes/g" -e "s/^BOOTPROTO=.*/BOOTPROTO=static/g" ifcfg-eth1 > ifcfg-eth1_new
-			echo"
-			IPADDR=192.168.56.101
-			NETMASK=255.255.255.0
-			" >> ifcfg-eth1_new
-			cp ifcfg-eth1_new ifcfg-eth1
+			echo "
+IPADDR=192.168.56.11
+NETMASK=255.255.255.0" >> ifcfg-eth1_new
+			mv ifcfg-eth1_new ifcfg-eth1
 			
 			cd /usr/src
 			wget ftp://ftp.freeradius.org/pub/freeradius/freeradius-server-2.1.12.tar.gz
@@ -51,13 +52,98 @@ case "$a" in
 			
 			cd /usr/local/etc/raddb
 			echo "
-			realm moonshot.nl{
-				type = radius
-				authhost = 192.168.56.102:1812
-				accthost = 192.168.56.102:1813
-				secret = testing123
-			}			
-			"
+realm moonshot.nl{
+	type = radius
+	authhost = 192.168.56.12:1812
+	accthost = 192.168.56.12:1813
+	secret = testing123
+}" >> proxy.conf
+
+		echo "
+client localradtest{
+	ipaddr = 192.168.56.11
+	secret = testing123
+	require_message_authenticator = no
+	nastype = other
+}
+
+client janet{
+	ipaddr = 192.168.56.14
+	secret = testing123
+	require_message_authenticator = no
+	nastype = other
+}" >> clients.conf
+			
+			cat eap.conf > eap.conf_old
+			sed "s/default_eap_type = md5/default_eap_type = ttls/g" eap.conf_old > eap_conf_new
+			mv eap_conf_new eap.conf
+			
+			break
+			;;
+			
+	"home")
+			echo "Installation in progress"
+			yum -y update
+			yum -y install make autoconf gcc wget openssl-devel
+
+			cd /etc/sysconfig/network-scripts
+			cat ifcfg-eth1 > ifcfg-eth1_old
+			sed "s/^ONBOOT=.*/ONBOOT=yes/g" -e "s/^BOOTPROTO=.*/BOOTPROTO=static/g" ifcfg-eth1 > ifcfg-eth1_new
+			echo "
+IPADDR=192.168.56.12
+NETMASK=255.255.255.0" >> ifcfg-eth1_new
+			mv ifcfg-eth1_new ifcfg-eth1
+
+			cd /usr/src
+			wget ftp://ftp.freeradius.org/pub/freeradius/freeradius-server-2.1.12.tar.gz
+			tar -xzf freeradius-server-2.1.12.tar.gz
+			rm -f freeradius-server-2.1.12.tar.gz
+			sleep 0.5
+
+			cd ./freeradius-server-2.1.12
+			./configure
+			make
+			make install
+
+			cd ./modules
+			git clone git://github.com/MoonshotNL/moonshotcode.git
+			mkdir rlm_moonshot
+			cp -vR ./moonshotcode/freeradius_smime/modules/* ./
+			cd ./modules
+			sleep 0.5
+			make
+			make install
+			cd ..
+			rm -rvf ./moonshotcode
+
+			cd /usr/local/etc/raddb
+
+			echo "
+client localradtest{
+	ipaddr = 192.168.56.12
+	secret = testing123
+	require_message_authenticator = no
+	nastype = other
+}
+
+client janet{
+	ipaddr = 192.168.56.14
+	secret = testing123
+	require_message_authenticator = no
+	nastype = other
+}
+
+client root_radius{
+	ipaddr = 192.168.56.11
+	secret = testing123
+	require_message_authenticator = no
+	nastype = other
+}" >> clients.conf
+
+			cat eap.conf > eap.conf_old
+			sed "s/default_eap_type = md5/default_eap_type = ttls/g" eap.conf_old > eap_conf_new
+			mv eap_conf_new eap.conf
+			
 			break
 			;;
 			
