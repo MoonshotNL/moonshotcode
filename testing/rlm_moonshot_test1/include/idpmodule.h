3284b41b39338aef6966e690411251ddb66d4b5a
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-06-11 20:48:02 +0200

    Removed the testing directory and created a seperate repository for it.

--- testing/rlm_moonshot_test1/include/idpmodule.h
+++
@@ -1,6 +0,0 @@
-#ifndef IDPMODULE_H
-#define IDPMODULE_H
-
-extern void idp_handle_requests(REQUEST *request);
-
-#endif
--- testing/rlm_moonshot_test2/include/request_handler_preproxy.h
+++
@@ -1,6 +0,0 @@
-#ifndef REQUEST_HANDLER_PREPROXY_H
-#define REQUEST_HANDLER_PREPROXY_H
-
-extern void proxy_handle_request(REQUEST *request);
-
-#endif
